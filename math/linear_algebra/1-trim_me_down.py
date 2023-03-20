bf41c1927772158fbd6bad4238c9935698e5fdd0
Camaltra <mickael.boillaud@gmail.com>
2023-03-20 18:28:23 +0100

    Check -- Cannot modify the source code bruh

--- math/linear_algebra/1-trim_me_down.py
+++ math/linear_algebra/1-trim_me_down.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 matrix = [[1, 3, 9, 4, 5, 8], [2, 4, 7, 3, 4, 0], [0, 3, 4, 6, 1, 5]]
+the_middle = []
 the_middle = [sub_matrix[2:4] for sub_matrix in matrix]
 print("The middle columns of the matrix are: {}".format(the_middle))
-"""The task ask to have 6 lines in our files"""
 """The task ask to have 6 lines in our files"""
