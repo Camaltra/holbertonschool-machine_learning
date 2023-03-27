f464d7ade749958315f413d7e95668b166eb5012
Camaltra <mickael.boillaud@gmail.com>
2023-03-27 19:08:09 +0200

    Check -- Task 17 can't rename the second param

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -3,21 +3,21 @@
 """Useless comment"""
 
 
-def poly_integral(polynome, primitive_const=0):
+def poly_integral(polynome, C=0):
     """
     Useless comment -- The name of the fuction is enough
     :param polynome: The given polynone
-    :param primitive_const: The integration constant
+    :param C: The integration constant
     :return: What the function say
     """
     if not isinstance(polynome, list) or len(polynome) < 1:
         return None
-    if not isinstance(primitive_const, int) and \
-            not isinstance(primitive_const, float):
+    if not isinstance(C, int) and \
+            not isinstance(C, float):
         return None
 
     primitive = [0] * (len(polynome) + 1)
     for idx in range(len(polynome) - 1, -1, -1):
         primitive[idx + 1] = polynome[idx] / (idx + 1)
-    primitive[0] = primitive_const
+    primitive[0] = C
     return primitive
