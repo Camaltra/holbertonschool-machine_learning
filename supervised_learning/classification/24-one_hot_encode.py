2d082de0908015e6c44e4e1bca7f102313596f30
Camaltra <mickael.boillaud@gmail.com>
2023-04-12 18:21:39 +0200

    Check -- Add checks

--- supervised_learning/classification/24-one_hot_encode.py
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -12,6 +12,10 @@ def one_hot_encode(Y, classes):
     :param classes: The max value of the list
     :return: The encoded matrix
     """
+    if not isinstance(Y, np.ndarray):
+        return None
+    if classes <= 2 or classes > len(Y):
+        return None
     one_hot_matrix = np.zeros((Y.size, classes))
     one_hot_matrix[np.arange(Y.size), Y] = 1
     return one_hot_matrix.T
