96b47affba4a55087cc58d1d0a94b3ffc0d3b2b1
Camaltra <mickael.boillaud@gmail.com>
2023-04-16 12:04:40 +0200

    Task -- Add the first task

---
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+
+"""Useless comments"""
+
+
+def create_placeholders(nx, classes):
+    """
+    Create placeholders tensor
+    :param nx: The number of feature columns in our data
+    :param classes: The number of classes in our classifier
+    :return: The two placeholders
+    """
+    x = tf.placeholder("float", [None, nx], name="x")
+    y = tf.placeholder("float", [None, classes], name="x")
+    return x, y
