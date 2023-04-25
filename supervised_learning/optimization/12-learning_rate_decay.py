39b00cc0f2d472f94543e1372330608971edfea2
Camaltra <mickael.boillaud@gmail.com>
2023-04-25 21:19:42 +0200

    Check -- Tensorflow version

--- supervised_learning/optimization/12-learning_rate_decay.py
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -7,12 +7,13 @@ import tensorflow as tf
 
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     """
-
-    :param alpha:
-    :param decay_rate:
-    :param global_step:
-    :param decay_step:
-    :return:
+    Calculate the new learning rate depend on a decay value and a step
+    as the alpha should be update in a step-wise fashion
+    :param alpha: The initial learning rate
+    :param decay_rate: The decay rate
+    :param global_step: The actual step
+    :param decay_step: The step-wise indicator
+    :return: The new learning rate
     """
     return tf.train.inverse_time_decay(alpha, global_step, decay_step,
-                                                 decay_rate, staircase=True)
+                                       decay_rate, staircase=True)
