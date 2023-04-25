c870d1cac5c72422c4a69c3f89bd50f3cc2cec36
Camaltra <mickael.boillaud@gmail.com>
2023-04-25 16:01:21 +0200

    Task -- Add the 5 task

---
+++ supervised_learning/optimization/5-momentum.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""Useless comment"""
+
+import numpy as np
+
+
+def update_variables_momentum(alpha, beta, var, grad, v):
+    """
+    Using the momentum to calculate the gradient descente
+    :param alpha: The learning rate
+    :param beta: The momentum weight
+    :param var: Contain the variable to be updated
+    :param grad: Contain the gradient of var
+    :param v: The previous irst moment of var
+    :return: The updated variable and the new moment, respectively
+    """
+    dw = beta * v + (1 - beta) * grad
+    return var - dw * alpha, dw
