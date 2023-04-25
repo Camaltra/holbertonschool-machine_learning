7e6ff4d3db998c530abf61428b2fd448695367ba
Camaltra <mickael.boillaud@gmail.com>
2023-04-25 21:35:31 +0200

    Task -- Add the task 13

---
+++ supervised_learning/optimization/13-batch_norm.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""Useless comment"""
+
+import numpy as np
+
+
+def batch_norm(Z, gamma, beta, epsilon):
+    """
+    Normalize a layer unactived input
+    Such as gamma * normalized_input + beta
+    :param Z: The layer unactived input
+    :param gamma: The gamma parameter
+    :param beta: The beta parameter
+    :param epsilon: A small number to avoid division by 0
+    :return: The normalized batch
+    """
+    mean, std = np.mean(Z, axis=0), np.std(Z, axis=0)
+    normilized_z = (Z - mean) / (std + epsilon)
+    return gamma * normilized_z + beta
