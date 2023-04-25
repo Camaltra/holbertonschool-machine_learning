98add912fcea658d2dd62af7624f4b7ab67a23ff
Camaltra <mickael.boillaud@gmail.com>
2023-04-25 21:17:16 +0200

    Check -- Tensorflow version

--- supervised_learning/optimization/12-learning_rate_decay.py
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -14,5 +14,5 @@ def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     :param decay_step:
     :return:
     """
-    return tf.compat.v1.train.inverse_time_decay(alpha, global_step, decay_step,
+    return tf.train.inverse_time_decay(alpha, global_step, decay_step,
                                                  decay_rate, staircase=True)
