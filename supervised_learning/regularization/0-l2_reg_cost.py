f51250a972d38fd849ca1ed1fbe75fea1b67e79d
Camaltra <mickael.boillaud@gmail.com>
2023-05-04 21:22:04 +0200

    Check -- Better Synthaxes

--- supervised_learning/regularization/0-l2_reg_cost.py
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -18,6 +18,6 @@ def l2_reg_cost(cost, lambtha, weights, L, m):
     """
     l2_cost = 0
     for layer_idx in range(L):
-        l2_cost += np.sum(weights.get("W{}").format(layer_idx + 1)**2)
+        l2_cost += np.sum(weights.get("W{}".format(layer_idx + 1))**2)
 
     return cost + (lambtha / (2 * m)) * l2_cost
