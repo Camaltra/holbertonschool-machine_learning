f72207240fb09822e62964146d6d588879e5e406
Camaltra <mickael.boillaud@gmail.com>
2023-05-09 20:58:39 +0200

    Checks -- Pycodestyle

--- supervised_learning/keras/8-train.py
+++ supervised_learning/keras/8-train.py
@@ -4,6 +4,7 @@
 
 import tensorflow.keras as K
 
+
 def train_model(network, data, labels, batch_size, epochs,
                 validation_data=None, early_stopping=False,
                 patience=0, learning_rate_decay=False,
