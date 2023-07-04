4791156db4487e4bcb5792e76350322893115139
Camaltra <mickael.boillaud@gmail.com>
2023-07-04 15:22:41 +0200

    Checks -- Fix Error handling

--- math/bayesian_prob/2-marginal.py
+++ math/bayesian_prob/2-marginal.py
@@ -33,7 +33,9 @@ def marginal(x, n, P, Pr):
         raise TypeError("P must be a 1D numpy.ndarray")
 
     if not isinstance(Pr, np.ndarray) or P.shape != Pr.shape:
-        raise ValueError("Pr must be a 1D numpy.ndarray")
+        raise TypeError(
+            "Pr must be a numpy.ndarray with the same shape as P"
+        )
 
     if np.any(P > 1) or np.any(P < 0):
         raise ValueError("All values in P must be in the range [0, 1]")
