84f57cc5c773dd9a4cf2bce202644e736bfc3aaa
Camaltra <mickael.boillaud@gmail.com>
2023-07-10 17:48:30 +0200

    Checks -- Import Error

--- unsupervised_learning/clustering/1-kmeans.py
+++ unsupervised_learning/clustering/1-kmeans.py
@@ -5,7 +5,26 @@
 
 
 import numpy as np
-initialize = __import__('0-initialize').initialize
+
+
+def initialize(X, k):
+    """
+    Init the K-mean clusters following a uniform proba law
+    :param X: The dataset
+    :param k: The number of clusters
+    :return: The coordonate of the cluster(s)
+    """
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None
+    if not isinstance(k, int) or k <= 0:
+        return None
+
+    X_min = np.min(X, axis=0)
+    X_max = np.max(X, axis=0)
+
+    cluster_center_coords = np.random.uniform(X_min, X_max, (k, X.shape[1]))
+
+    return cluster_center_coords
 
 
 def compute_centroid_distance(X, centroids_coords):
@@ -26,7 +45,11 @@ def kmeans(X, k, iterations=1000):
     :param iterations: The number of iterations
     :return: The computed cluster centers | The class for each points
     """
-    if not isinstance(X, np.ndarray) or k <= 0 or iterations <= 0:
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None
+    if not isinstance(k, int) or k <= 0:
+        return None
+    if not isinstance(iterations, np.int) or iterations <= 0:
         return None, None
 
     centroids = initialize(X, k)
