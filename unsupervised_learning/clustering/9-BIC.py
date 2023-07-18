d675b7e3acedffdf30dd4353a6bf462300c8de51
Camaltra <mickael.boillaud@gmail.com>
2023-07-18 08:47:15 +0200

    Checks -- Revert -- Get the teacher solution

--- unsupervised_learning/clustering/9-BIC.py
+++ unsupervised_learning/clustering/9-BIC.py
@@ -8,58 +8,34 @@ import numpy as np
 expectation_maximization = __import__('8-EM').expectation_maximization
 
 
-
 def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
-    """
-    Use the BIC algos to find the best K parameter the EM algo
-    :param X: The dataset
-    :param kmin: kmin
-    :param kmax: kmax
-    :param iterations: The number of iteration the EM algo
-    :param tol: The tol the EM algo
-    :param verbose: The verbose the EM aglo
-    :return: Best K, Best res and the history of both l and b
-    """
-    if type(X) != np.ndarray or len(X.shape) != 2:
+    """calculates BIC over various """
+    if type(X) is not np.ndarray or X.ndim != 2:
         return None, None, None, None
-    if type(kmin) != int or kmin < 1:
+    n, d = X.shape
+    if type(kmin) is not int or kmin != int(kmin) or kmin < 1:
         return None, None, None, None
     if kmax is None:
-        kmax = X.shape[0]
-    if type(kmax) != int or kmax < 1:
+        kmax = n
+    if type(kmax) is not int or kmax != int(kmax) or kmax < 1:
         return None, None, None, None
     if kmax <= kmin:
         return None, None, None, None
-    if type(iterations) != int or iterations < 1:
+    if type(iterations) is not int or iterations != int(iterations) or iterations < 1:
         return None, None, None, None
-    if type(tol) != float or tol < 0:
+    if type(tol) is not float or tol < 0:
         return None, None, None, None
-    if type(verbose) != bool:
+    if type(verbose) is not bool:
         return None, None, None, None
-
-    n, d = X.shape
-    k_values_list = []
-    results_list = []
-    log_likelihood_list = []
-    bic_value_list = []
-
+    b = np.zeros(kmax + 1 - kmin)
+    l = np.zeros(kmax + 1 - kmin)
+    results = []
     for k in range(kmin, kmax + 1):
-        pi, m, S, _, log_likelihood = expectation_maximization(
-            X, k, iterations, tol, verbose)
-
-        p = d * k + (d * k * (d + 1) / 2) + k - 1
-        BIC = p * np.log(n) - 2 * log_likelihood
-
-        k_values_list.append(k)
-        results_list.append((pi, m, S))
-        log_likelihood_list.append(log_likelihood)
-        bic_value_list.append(BIC)
-
-        log_likelihood_array = np.array(log_likelihood_list)
-        bic_value_array = np.array(bic_value_list)
-        index = np.argmin(bic_value_array)
-
-        best_k = k_values_list[index]
-        best_result = results_list[index]
-
-    return best_k, best_result, log_likelihood_array, bic_value_array
+        pi, m, S, _, l[k - kmin] = expectation_maximization(X, k, iterations=iterations, tol=tol, verbose=verbose)
+        results.append((pi, m, S))
+        p = k * (d + 2) * (d + 1) / 2 - 1
+        b[k - kmin] = p * np.log(n) - 2 * l[k - kmin]
+    amin = np.argmin(b)
+    best_k = amin + kmin
+    best_result = results[amin]
+    return best_k, best_result, l, b
