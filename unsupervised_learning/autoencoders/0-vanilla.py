02e08d0cf8b4866ee7c1e07ddf5d8f4c4e05d120
Camaltra <mickael.boillaud@gmail.com>
2023-08-03 17:30:16 +0200

    Checks -- Pycodestyle validation

--- unsupervised_learning/autoencoders/0-vanilla.py
+++ unsupervised_learning/autoencoders/0-vanilla.py
@@ -15,21 +15,44 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
     :param latent_dims: Latend dimensions
     :return: Compiled vanilla autoencoder
     """
-    encoder_input = keras.layers.Input(shape=(input_dims, ), name="encoder_input")
+    encoder_input = keras.layers.Input(
+        shape=(input_dims,),
+        name="encoder_input",
+    )
     encoder_layer = encoder_input
     for units in hidden_layers:
-        encoder_layer = keras.layers.Dense(units=units, activation="relu")(encoder_layer)
-    encoder_output = keras.layers.Dense(units=latent_dims, activation="relu", name="encoder_latent")(encoder_layer)
+        encoder_layer = keras.layers.Dense(units=units, activation="relu")(
+            encoder_layer
+        )
+    encoder_output = keras.layers.Dense(
+        units=latent_dims, activation="relu", name="encoder_latent"
+    )(encoder_layer)
 
-    encoder_model = keras.models.Model(encoder_input, encoder_output, name="encoder")
+    encoder_model = keras.models.Model(
+        encoder_input,
+        encoder_output,
+        name="encoder",
+    )
 
-    decoder_input = keras.layers.Input(shape=(latent_dims, ), name="decoder_input")
+    decoder_input = keras.layers.Input(
+        shape=(latent_dims,),
+        name="decoder_input",
+    )
     decoder_layer = decoder_input
     for units in reversed(hidden_layers):
-        decoder_layer = keras.layers.Dense(units=units, activation="relu")(decoder_layer)
-    decoder_output = keras.layers.Dense(units=input_dims, activation="sigmoid")(decoder_layer)
+        decoder_layer = keras.layers.Dense(units=units, activation="relu")(
+            decoder_layer
+        )
+    decoder_output = keras.layers.Dense(
+        units=input_dims,
+        activation="sigmoid",
+    )(decoder_layer)
 
-    decoder_model = keras.models.Model(decoder_input, decoder_output, name="decoder")
+    decoder_model = keras.models.Model(
+        decoder_input,
+        decoder_output,
+        name="decoder",
+    )
 
     auto = keras.models.Model(
         encoder_model.input,
