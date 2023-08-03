1313e2100e38fd97dc805f777a7d8693d524cca0
Camaltra <mickael.boillaud@gmail.com>
2023-08-03 17:26:04 +0200

    Checks -- Change from Sequential to Model

--- unsupervised_learning/autoencoders/0-vanilla.py
+++ unsupervised_learning/autoencoders/0-vanilla.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 
+"""userless comment"""
+
+
 import tensorflow.keras as keras
 
 
@@ -12,40 +15,25 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
     :param latent_dims: Latend dimensions
     :return: Compiled vanilla autoencoder
     """
-    encoder = (
-        [
-            keras.layers.Dense(
-                units=hidden_layers[0], input_dim=input_dims, activation="relu"
-            )
-        ]
-        + [
-            keras.layers.Dense(units=units, activation="relu")
-            for units in hidden_layers[1:]
-        ]
-        + [keras.layers.Dense(units=latent_dims, activation="relu")]
-    )
+    encoder_input = keras.layers.Input(shape=(input_dims, ), name="encoder_input")
+    encoder_layer = encoder_input
+    for units in hidden_layers:
+        encoder_layer = keras.layers.Dense(units=units, activation="relu")(encoder_layer)
+    encoder_output = keras.layers.Dense(units=latent_dims, activation="relu", name="encoder_latent")(encoder_layer)
 
-    decoder = (
-        [
-            keras.layers.Dense(
-                units=hidden_layers[-1],
-                input_dim=latent_dims,
-                activation="relu",
-            )
-        ]
-        + [
-            keras.layers.Dense(units=units, activation="relu")
-            for units in hidden_layers[-2:-1:-1]
-        ]
-        + [keras.layers.Dense(units=input_dims, activation="sigmoid")]
-    )
+    encoder_model = keras.models.Model(encoder_input, encoder_output, name="encoder")
 
-    encoder_model = keras.models.Sequential(encoder)
-    decoder_model = keras.models.Sequential(decoder)
+    decoder_input = keras.layers.Input(shape=(latent_dims, ), name="decoder_input")
+    decoder_layer = decoder_input
+    for units in reversed(hidden_layers):
+        decoder_layer = keras.layers.Dense(units=units, activation="relu")(decoder_layer)
+    decoder_output = keras.layers.Dense(units=input_dims, activation="sigmoid")(decoder_layer)
+
+    decoder_model = keras.models.Model(decoder_input, decoder_output, name="decoder")
 
     auto = keras.models.Model(
         encoder_model.input,
-        decoder_model(encoder_model.output),
+        decoder_model(encoder_model(encoder_model.input)),
     )
 
     auto.compile(optimizer=keras.optimizers.Adam(), loss="binary_crossentropy")
--- unsupervised_learning/autoencoders/1-sparse.py
+++ unsupervised_learning/autoencoders/1-sparse.py
@@ -13,46 +13,25 @@ def autoencoder(input_dims, hidden_layers, latent_dims, lambtha):
     :param lambtha: The rate for the l2 regularizer
     :return: Compiled sparse autoencoder
     """
-    encoder = (
-        [
-            keras.layers.Dense(
-                units=hidden_layers[0], input_dim=input_dims, activation="relu"
-            )
-        ]
-        + [
-            keras.layers.Dense(units=units, activation="relu")
-            for units in hidden_layers[1:]
-        ]
-        + [
-            keras.layers.Dense(
-                units=latent_dims,
-                activation="relu",
-                activity_regularizer=keras.regularizers.l1(lambtha),
-            )
-        ]
-    )
+    encoder_input = keras.layers.Input(shape=(input_dims, ), name="encoder_input")
+    encoder_layer = encoder_input
+    for units in hidden_layers:
+        encoder_layer = keras.layers.Dense(units=units, activation="relu")(encoder_layer)
+    encoder_output = keras.layers.Dense(units=latent_dims, activation="relu", activity_regularizer=keras.regularizers.l1(lambtha), name="encoder_latent")(encoder_layer)
 
-    decoder = (
-        [
-            keras.layers.Dense(
-                units=hidden_layers[-1],
-                input_dim=latent_dims,
-                activation="relu",
-            )
-        ]
-        + [
-            keras.layers.Dense(units=units, activation="relu")
-            for units in hidden_layers[-2:-1:-1]
-        ]
-        + [keras.layers.Dense(units=input_dims, activation="sigmoid")]
-    )
+    encoder_model = keras.models.Model(encoder_input, encoder_output, name="encoder")
 
-    encoder_model = keras.models.Sequential(encoder)
-    decoder_model = keras.models.Sequential(decoder)
+    decoder_input = keras.layers.Input(shape=(latent_dims, ), name="decoder_input")
+    decoder_layer = decoder_input
+    for units in reversed(hidden_layers):
+        decoder_layer = keras.layers.Dense(units=units, activation="relu")(decoder_layer)
+    decoder_output = keras.layers.Dense(units=input_dims, activation="sigmoid")(decoder_layer)
+
+    decoder_model = keras.models.Model(decoder_input, decoder_output, name="decoder")
 
     auto = keras.models.Model(
         encoder_model.input,
-        decoder_model(encoder_model.output),
+        decoder_model(encoder_model(encoder_model.input)),
     )
 
     auto.compile(optimizer=keras.optimizers.Adam(), loss="binary_crossentropy")
