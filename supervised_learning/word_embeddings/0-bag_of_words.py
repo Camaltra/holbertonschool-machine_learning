580c7d301b541e66347353963e00a922814a6aea
Camaltra <mickael.boillaud@gmail.com>
2023-08-21 17:49:48 +0200

    Checks -- Disable auto sort | Change output to np.ndarray(int)

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -32,13 +32,15 @@ class CountVectorizer:
         return word.lower()
 
     @staticmethod
-    def format_vocab(vocab):
+    def format_vocab(vocab, sort_values):
         """
         Fomart the vocab in a dict {word: idx}
         :param vocab: The vocab
         :return: The formatted vocab
         """
-        return {word: idx for idx, word in enumerate(sorted(vocab))}
+        if sort_values:
+            return {word: idx for idx, word in enumerate(sorted(vocab))}
+        return {word: idx for idx, word in enumerate(vocab)}
 
     def process_sentence(self, sentence):
         """
@@ -74,9 +76,11 @@ class CountVectorizer:
         :param y: The label if exist
         :return: Self
         """
+        sort_values = False
         if self.vocab is None:
+            sort_values = True
             self.vocab = self.create_vocab(X)
-        self.processed_formatted_vocab = self.format_vocab(self.vocab)
+        self.processed_formatted_vocab = self.format_vocab(self.vocab, sort_values)
         self.features = list(self.processed_formatted_vocab.keys())
         return self
 
@@ -86,7 +90,7 @@ class CountVectorizer:
         :param X: The data
         :return: The transformed data
         """
-        bag_of_word = np.zeros((len(X), len(self.vocab)))
+        bag_of_word = np.zeros((len(X), len(self.vocab)), dtype=np.int)
         for idx, sentence in enumerate(X):
             processed_sentence = self.process_sentence(sentence)
             for word in processed_sentence:
