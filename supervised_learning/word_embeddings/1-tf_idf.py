e7460091cd63e554c14905378f64941d54394abb
Camaltra <mickael.boillaud@gmail.com>
2023-08-22 11:38:41 +0200

    Tasks -- Add the tasks 1

---
+++ supervised_learning/word_embeddings/1-tf_idf.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+
+"""useless comment"""
+
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+def tf_idf(sentences, vocab=None):
+    """
+    Create the matrix of TF - IDF
+    :param sentences: A list of sentences
+    :param vocab: The vocab is needed
+    :return: The matrix, the vocab
+    """
+    tf_idf_vectorizer = TfidfVectorizer(vocabulary=vocab)
+    output = tf_idf_vectorizer.fit_transform(sentences)
+
+    return output.toarray(), list(tf_idf_vectorizer.vocabulary_.keys())
