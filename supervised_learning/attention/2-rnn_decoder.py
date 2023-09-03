bf4f76806e5480e7803c4a6cad54aa1e8ffe2b5b
Camaltra <mickael.boillaud@gmail.com>
2023-09-03 15:29:14 +0200

    Checks -- PLS NEVER GO HOLBERTON SCHOOL

--- supervised_learning/attention/2-rnn_decoder.py
+++ supervised_learning/attention/2-rnn_decoder.py
@@ -7,7 +7,7 @@
 import numpy as np
 import tensorflow as tf
 
-SelfAttention = __import__("1-self_attention").SelfAttention
+SelfAttention = __import__('1-self_attention').SelfAttention
 
 
 class RNNDecoder(tf.keras.layers.Layer):
