d09b6df1c8acd5e4509a3027220f7762fc0cee19
Camaltra <mickael.boillaud@gmail.com>
2023-09-03 15:33:15 +0200

    Checks -- PLS NEVER GO HOLBERTON SCHOOL

--- supervised_learning/attention/5-sdp_attention.py
+++ supervised_learning/attention/5-sdp_attention.py
@@ -15,15 +15,16 @@ def sdp_attention(Q, K, V, mask=None):
     :param mask: The mask if any
     :return: The attention and the weight
     """
+    dk = tf.cast(tf.shape(K)[-1], tf.float32)
     if mask is not None:
         mask *= -1e9
         weight = tf.math.softmax(
-            (tf.matmul(Q, K, transpose_b=True) + mask) / tf.math.sqrt(Q.shape[-1])
+            (tf.matmul(Q, K, transpose_b=True) + mask) / tf.math.sqrt(dk)
         )
 
     else:
         weight = tf.math.softmax(
-            tf.matmul(Q, K, transpose_b=True) / tf.math.sqrt(Q.shape[-1])
+            tf.matmul(Q, K, transpose_b=True) / tf.math.sqrt(dk)
         )
 
     attention = tf.matmul(weight, V)
