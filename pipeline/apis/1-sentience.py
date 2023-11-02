9c01e3801e9cd4b2cc1c1208c72c64ee678c3e0f
Camaltra <mickael.boillaud@gmail.com>
2023-11-02 16:18:39 +0100

    Tasks -- Add project

--- pipeline/apis/1-sentience.py
+++ pipeline/apis/1-sentience.py
@@ -9,23 +9,22 @@ def sentientPlanets():
     """
     Useless comment
     """
-    data = requests.get("https://swapi-api.hbtn.io/api/species").json()
+    url = "https://swapi-api.hbtn.io/api/species"
     sentient_planets = []
-    visited_homeworld_url = set()
-    while data.get("next") is not None:
+    while url is not None:
+        data = requests.get(url).json()
         species = data.get("results")
         for specie in species:
             if specie.get('designation') == 'sentient' or \
                     specie.get('classification') == 'sentient':
                 homeworld_url = specie.get("homeworld")
-                if homeworld_url in visited_homeworld_url \
-                        or homeworld_url is None:
+
+                if homeworld_url is None:
                     continue
                 sentient_planets.append(
                     requests.get(homeworld_url).json().get("name")
                 )
-                visited_homeworld_url.add(homeworld_url)
 
-        data = requests.get(data.get("next")).json()
+        url = data.get("next")
 
     return sentient_planets
